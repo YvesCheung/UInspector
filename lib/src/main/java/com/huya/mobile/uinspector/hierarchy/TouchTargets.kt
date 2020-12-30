43a680d645815e7c5dff0950f5599843f2a28426
YvesCheung <975135274@qq.com>
2020-12-30 14:06:36 +0800

    Finish logic to find the touch targets

--- lib/src/main/java/com/huya/mobile/uinspector/hierarchy/TouchTargets.kt
+++ lib/src/main/java/com/huya/mobile/uinspector/hierarchy/TouchTargets.kt
@@ -1,11 +1,16 @@
 package com.huya.mobile.uinspector.hierarchy
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import com.huya.mobile.uinspector.util.LibName
+import com.huya.mobile.uinspector.util.isOnView
 import java.lang.reflect.Field
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * @author YvesCheung
@@ -15,41 +20,76 @@ import java.util.*
 @SuppressLint("PrivateApi")
 internal object TouchTargets {
 
-    private val firstTouchTarget: Field by lazy(LazyThreadSafetyMode.NONE) {
+    val firstTouchTarget: Field by lazy(LazyThreadSafetyMode.NONE) {
         val f = ViewGroup::class.java.getDeclaredField("mFirstTouchTarget")
         f.isAccessible = true
         f
     }
 
-    private val touchTargetChild: Field by lazy(LazyThreadSafetyMode.NONE) {
+    val touchTargetChild: Field by lazy(LazyThreadSafetyMode.NONE) {
         val cls = Class.forName("android.view.ViewGroup\$TouchTarget")
         val f = cls.getDeclaredField("child")
         f.isAccessible = true
         f
     }
 
-    fun findFirstTouchTarget(parent: View?): View? {
+    fun findFirstTouchTargets(parent: View, touchEvent: MotionEvent): List<View> {
+        val queue = LinkedList<View>()
+        var current: View? = parent
+        while (current != null) {
+            queue.add(current)
+
+            current = findFirstTouchTarget(current, touchEvent)
+        }
+        return queue
+    }
+
+    fun findFirstTouchTarget(parent: View?, touchEvent: MotionEvent): View? {
         if (parent is ViewGroup) {
-            try {
+            return try {
                 val touchTarget = firstTouchTarget.get(parent)
                 if (touchTarget != null) {
-                    return touchTargetChild.get(touchTarget) as? View
+                    touchTargetChild.get(touchTarget) as? View
+                } else {
+                    findTouchTargetByEvent(parent, touchEvent)
                 }
             } catch (e: Throwable) {
-                Log.e("UInspector", e.toString())
+                Log.e(LibName, e.toString())
+                findTouchTargetByEvent(parent, touchEvent)
             }
         }
         return null
     }
 
-    fun findFirstTouchTargets(parent: View?): List<View> {
-        val queue = LinkedList<View>()
-        var current = parent
-        while (current != null) {
-            queue.add(current)
+    fun findTouchTargetByEvent(parent: ViewGroup, touchEvent: MotionEvent): View? {
+        if (parent.childCount <= 0) return null
 
-            current = findFirstTouchTarget(current)
+        val dispatchTouchOrder = ArrayList<View>(parent.childCount)
+        for (drawIndex in 0 until parent.childCount) {
+
+            //todo: check isChildrenDrawingOrderEnabled
+            var childIndex = parent.getChildDrawingOrder(drawIndex)
+            if (childIndex !in 0 until parent.childCount) childIndex = drawIndex
+
+            val child = parent.getChildAt(childIndex)
+
+            var insertIndex: Int = drawIndex
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                // insert ahead of any Views with greater Z
+                val currentZ = child.z
+                while (insertIndex > 0 && dispatchTouchOrder[insertIndex - 1].z > currentZ) {
+                    insertIndex--
+                }
+            }
+            dispatchTouchOrder.add(insertIndex, child)
         }
-        return queue
+
+        for (child in dispatchTouchOrder.asReversed()) { //from top to bottom
+            if (isOnView(touchEvent, child)) {
+                return child
+            }
+        }
+
+        return null
     }
 }
