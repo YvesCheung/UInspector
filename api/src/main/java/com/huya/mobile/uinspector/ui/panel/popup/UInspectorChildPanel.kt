a587b2c7edc6cdfde2b0359f46a0ebef4c0a4b2c
YvesCheung <975135274@qq.com>
2021-01-03 18:21:00 +0800

    Add Javadoc

--- api/src/main/java/com/huya/mobile/uinspector/ui/panel/popup/UInspectorChildPanel.kt
+++ api/src/main/java/com/huya/mobile/uinspector/ui/panel/popup/UInspectorChildPanel.kt
@@ -11,13 +11,25 @@ import androidx.annotation.MainThread
 @MainThread
 interface UInspectorChildPanel {
 
+    /**
+     * Determine the position of this panel
+     */
     val priority: Int
 
     val title: CharSequence
 
+    /**
+     * Only called once when this panel is created on the screen
+     */
     fun onCreateView(context: Context): View
 
+    /**
+     * Only called once when this panel is going to be destroyed
+     */
     fun onDestroyView() {}
 
+    /**
+     * Called every time when the selected panel change
+     */
     fun onUserVisibleHint(visible: Boolean) {}
 }
--- api/src/main/java/com/huya/mobile/uinspector/ui/panel/popup/UInspectorChildPanelService.kt
+++ api/src/main/java/com/huya/mobile/uinspector/ui/panel/popup/UInspectorChildPanelService.kt
@@ -1,10 +1,16 @@
 package com.huya.mobile.uinspector.ui.panel.popup
 
 /**
+ * Using Java SPI mechanism
+ *
  * @author YvesCheung
  * 2020/12/31
  */
 interface UInspectorChildPanelService {
 
+    /**
+     * To initialize your own [UInspectorChildPanel].
+     * Load them into the framework.
+     */
     fun createPanels(): Set<UInspectorChildPanel>
 }
