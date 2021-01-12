da886910b941800a432749bbdd0fa4bf2937943d
YvesCheung <975135274@qq.com>
2021-01-12 14:57:06 +0800

    Add annotation

--- optional/glide/src/main/java/com/huya/mobile/uinspector/optional/glide/GlideViewParser.kt
+++ optional/glide/src/main/java/com/huya/mobile/uinspector/optional/glide/GlideViewParser.kt
@@ -3,6 +3,7 @@ package com.huya.mobile.uinspector.optional.glide
 import android.view.View
 import com.bumptech.glide.request.Request
 import com.huya.mobile.uinspector.impl.properties.view.ViewPropertiesParser
+import com.yy.mobile.whisper.Output
 
 /**
  * @author YvesCheung
@@ -10,7 +11,7 @@ import com.huya.mobile.uinspector.impl.properties.view.ViewPropertiesParser
  */
 class GlideViewParser(view: View, private val request: Request) : ViewPropertiesParser<View>(view) {
 
-    override fun parse(props: MutableMap<String, Any?>) {
+    override fun parse(@Output props: MutableMap<String, Any?>) {
         super.parse(props)
         GlideRequestParser(view.context, request).parse(props)
     }
