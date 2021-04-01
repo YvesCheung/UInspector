01b2132ec62bac0e3231aeb1efb72fe7d1e9f798
YvesCheung <975135274@qq.com>
2021-04-01 16:46:44 +0800

    Add MultiType support

---
+++ optional/multitype/src/main/java/com/huya/mobile/uinspector/optional/multitype/MultiTypeExtraInfoService.kt
@@ -0,0 +1,50 @@
+package com.huya.mobile.uinspector.optional.multitype
+
+import android.app.Activity
+import android.content.Context
+import android.text.SpannableStringBuilder
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.huya.mobile.uinspector.impl.hierarchy.extra.HierarchyExtraInfo
+import com.huya.mobile.uinspector.impl.hierarchy.extra.HierarchyExtraInfoService
+import com.huya.mobile.uinspector.util.newLine
+import com.huya.mobile.uinspector.util.withBold
+import com.huya.mobile.uinspector.util.withColor
+import me.drakeet.multitype.MultiTypeAdapter
+
+/**
+ * @author YvesCheung
+ * 2021/4/1
+ */
+class MultiTypeExtraInfoService : HierarchyExtraInfoService {
+
+    override fun create(activity: Activity, targetView: View): Set<HierarchyExtraInfo> {
+        return setOf(ViewBinderInfo(activity))
+    }
+
+    private class ViewBinderInfo(val context: Context) : HierarchyExtraInfo {
+
+        override fun beforeHierarchy(index: Int, view: View, s: SpannableStringBuilder) {
+            val maybeRecyclerView = view.parent
+            if (maybeRecyclerView is RecyclerView) {
+                val layoutManager = maybeRecyclerView.layoutManager ?: return
+                val maybeMultiTypeAdapter = maybeRecyclerView.adapter
+                if (maybeMultiTypeAdapter is MultiTypeAdapter) {
+                    val viewBinder = maybeMultiTypeAdapter.typePool
+                        .getItemViewBinder(layoutManager.getItemViewType(view))
+
+                    s.withColor(context) {
+                        withBold {
+                            newLine(index) {
+                                append(
+                                    viewBinder::class.java.canonicalName
+                                        ?: viewBinder::class.java.name
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
